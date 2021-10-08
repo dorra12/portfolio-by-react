9f6e75acfbff9a81e13c6f9e1cf5862a06399ffc
Dorra <dorra.hbaieb@gmail.com>
2021-10-08 16:39:52 +0200

    add HomePage Component

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -9,11 +9,11 @@ function MainPage() {
                 <span></span>
             </div>
             <div className="container">
-                <div className="title title_fz16 promo__subtitle">My name is Dorra Habaieb</div>
-                <h1 className="title title_fz48 promo__title">I'm a web-developer from Luxembourg</h1>
-                <div className="promo__btns">
-                    <a href="#" className="promo__link btn">Portfolio</a>
-                    <a href="#" className="promo__link btn">About me</a>
+                <div className="title title_fz16 mainPage__subtitle">My name is Dorra Habaieb</div>
+                <h1 className="title title_fz48 mainPage__title">I'm a web-developer from Luxembourg</h1>
+                <div className="mainPage__btns">
+                    <a href="#" className="mainPage__link btn">Portfolio</a>
+                    <a href="#" className="mainPage__link btn">About me</a>
                 </div>
             </div>
         </section>
