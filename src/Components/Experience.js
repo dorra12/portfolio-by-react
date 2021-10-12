66a0336092aa2aac7ee5b41e3ef5c20aa0f7d1d0
Dorra <dorra.hbaieb@gmail.com>
2021-10-12 10:37:33 +0200

    Create Experience Component

---
+++ src/Components/Experience.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import '../Styles/Experience.scss'
+
+function Experience() {
+    return (
+    <section className="experience">
+        <div className="container">
+            <h2 className="title title_fz16 title__section-title">Experience</h2>
+            <div className="title title_fz36 experience__subtitle">Lorem Upsum</div>
+            
+            <div className="divider"></div>
+            <div className="experience__wrapper">
+                <div className="experience__column">
+                    <h3 className="title title_fz20 resume__column-title">Education</h3>
+                    <ul>
+                        <li>
+                            <div className="experience__item">
+                                <div className="experience__item-head">
+                                    <div className="experience__item-icon">
+                                        <img src="" alt=""></img>
+                                    </div>
+                                    <h4 className="title title_fz14">Université Libre de Tunis</h4>
+                                    <div className="experience__item-location">Biological Engineer</div>
+                                </div>
+                                <div class="experience__item-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="experience__item">
+                                <div className="experience__item-head">
+                                    <div className="experience__item-icon">
+                                        <img src="" alt=""></img>
+                                    </div>
+                                    <h4 className="title title_fz14">Université Libre de Tunis</h4>
+                                    <div className="experience__item-location">Biological Engineer</div>
+                                </div>
+                                <div class="experience__item-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="experience__column">
+                    <h3 className="title title_fz20 resume__column-title">Work experience</h3>
+                    <ul>
+                        <li>
+                            <div className="experience__item">
+                                <div className="experience__item-head">
+                                    <div className="experience__item-icon">
+                                        <img src="" alt=""></img>
+                                    </div>
+                                    <h4 className="title title_fz14">Université Libre de Tunis</h4>
+                                    <div className="experience__item-location">Biological Engineer</div>
+                                </div>
+                                <div class="experience__item-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="experience__item">
+                                <div className="experience__item-head">
+                                    <div className="experience__item-icon">
+                                        <img src="" alt=""></img>
+                                    </div>
+                                    <h4 className="title title_fz14">Université Libre de Tunis</h4>
+                                    <div className="experience__item-location">Biological Engineer</div>
+                                </div>
+                                <div class="experience__item-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>  
+
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Experience
